271b79a8df67d2f11ca9bce9e306001ed801b277
mwosiek <malgorzatawosiek96@gmail.com>
2021-03-24 11:48:58 +0100

    RecipeController added

--- src/main/java/io/github/mwosiek/repositories/SqlRecipeRepository.java
+++ src/main/java/io/github/mwosiek/repositories/SqlRecipeRepository.java
@@ -1,14 +1,13 @@
-package io.github.mwosiek.model;
-
-
+package io.github.mwosiek.repositories;
+import io.github.mwosiek.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface SqlRecipeRepository extends RecipeRepository, JpaRepository<Recipe,Integer> {
+}
 
 //klasa, która służy do komunikacji z bazą danych.
 
 /*@RepositoryRestResource - tutaj mówimy springowi-to jest repozytorium,
 tutaj są metody z których ty masz korzystac.*/
-
-@RepositoryRestResource
-interface RecipeRepository extends JpaRepository<Recipe,Integer> {
-}
